01000ea85b43527ecf916e24e861fe0644049c0b
Jeff Barriault <barriauj@tamug.edu>
2013-09-18 13:25:43 -0500

    change to production cas server

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :cas, host: 'cas-dev.tamu.edu', ssl: true, login_url: '/cas/login', logout_url: '/cas/logout', service_validate_url: '/cas/serviceValidate'
+  provider :cas, host: 'cas.tamu.edu', ssl: true, login_url: '/cas/login', logout_url: '/cas/logout', service_validate_url: '/cas/serviceValidate'
 end
