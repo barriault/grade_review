2d961ec69fbd54d50fbf738a44d8d047619cf0be
Jeff Barriault <barriauj@tamug.edu>
2013-09-20 11:29:50 -0500

    fix OmniAuth to use cas.tamu.edu for production and cas-dev.tamu.edu for development/test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :cas, host: 'cas.tamu.edu', ssl: true, login_url: '/cas/login', logout_url: '/cas/logout', service_validate_url: '/cas/serviceValidate'
+  if Rails.env.production?
+    provider :cas, host: 'cas.tamu.edu', ssl: true, login_url: '/cas/login', logout_url: '/cas/logout', service_validate_url: '/cas/serviceValidate'
+  else
+    provider :cas, host: 'cas-dev.tamu.edu', ssl: true, login_url: '/cas/login', logout_url: '/cas/logout', service_validate_url: '/cas/serviceValidate'
+  end
 end
