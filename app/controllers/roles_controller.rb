80243abc14533c7091e304ba74b5d5afbf211afe
Jeff Barriault <barriauj@tamug.edu>
2013-09-20 14:14:39 -0500

    move roles off of active_scaffold

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,11 +1,40 @@
 class RolesController < ApplicationController
-  
   before_filter :authenticate_user!
+  load_and_authorize_resource
+
+  def index
+  end
+
+  def show
+  end
   
-  active_scaffold :role do |config|
-    config.label = "All Roles"
-    config.columns = [:name]
-    list.sorting = {:created_at => 'DESC'}
+  def update
+    if @role.update_attributes(params[:role])
+      redirect_to roles_path, :notice => "Role updated."
+    else
+      redirect_to roles_path, :alert => "Unable to update role."
+    end
+  end
+    
+  def destroy
+    @role.destroy
+    redirect_to roles_path, :notice => "Role deleted."
+  end
+  
+  def edit
+  end
+  
+  def new
+    @role = Role.new
+  end
+  
+  def create
+    @role = Role.new(params[:role])
+    if @role.save
+      redirect_to roles_path, notice: 'Role was successfully created.'
+    else
+      render action: "new"
+    end
   end
   
 end
