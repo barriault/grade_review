3b868e4096eb4de9dce9da469ab4c37b7a14e83a
Jeff Barriault <barriauj@tamug.edu>
2013-09-20 11:28:47 -0500

    Add deploy task to seed database

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,13 @@ ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
+namespace :db do
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
 namespace :deploy do
   task :start do; end
   task :stop do; end
