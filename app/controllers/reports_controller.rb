04ed7a49aa35975b1a96a6d7b063d1d2c9f93574
Jeff Barriault <barriauj@tamug.edu>
2013-10-22 12:36:01 -0500

    Last time, I promise . . .

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -91,7 +91,7 @@ class ReportsController < ApplicationController
   
   def departmental_suspensions
     @students = Student.accessible_by(current_ability)
-      .where("initial_status = 'Probation' AND final_status = 'Departtmental Suspension'")
+      .where("initial_status = 'Probation' AND final_status = 'Departmental Suspension'")
       .order("major, classification, last_name, first_name")
       
     respond_to do |format|
