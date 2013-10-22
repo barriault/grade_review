26544c2073f6ff62777f1078a239a71b23868466
Jeff Barriault <barriauj@tamug.edu>
2013-10-22 12:30:38 -0500

    one more try . . .

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -91,7 +91,7 @@ class ReportsController < ApplicationController
   
   def departmental_suspensions
     @students = Student.accessible_by(current_ability)
-      .where("initial_status = 'Probation' AND final_status = 'Deptmental Susp'")
+      .where("initial_status = 'Probation' AND final_status = 'Deptmental Suspension'")
       .order("major, classification, last_name, first_name")
       
     respond_to do |format|
