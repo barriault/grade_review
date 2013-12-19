bbecf04e931bcec8a02ff4a0ab507c168b90d80e
Jeff Barriault <barriauj@tamug.edu>
2013-12-19 08:08:27 -0600

    revert

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,7 +14,7 @@ class Ability
     end
     
     if user.has_role? :super_user
-      # can :manage_student, Student
+      can :manage_student, Student
       can :manage_appeal, Appeal
       can :manage_address, Address
     end
@@ -25,43 +25,43 @@ class Ability
     end
     
     if user.has_role? :marb
-      # can :manage_student, Student, :major => ['MARB', 'MARF']
+      can :manage_student, Student, :major => ['MARB', 'MARF']
       can :recommend_appeal, AppealRecommendation, :major => ['MARB', 'MARF']
       can :manage_address, Address, :major => ['MARB', 'MARF']
     end
     
     if user.has_role? :mare
-      # can :manage_student, Student, :major => ['MARR', 'MARE']
+      can :manage_student, Student, :major => ['MARR', 'MARE']
       can :recommend_appeal, AppealRecommendation, :major => ['MARR', 'MARE']
       can :manage_address, Address, :major => ['MARR', 'MARE']
     end
     
     if user.has_role? :mars
-      # can :manage_student, Student, :major => ['MARS', 'OCRE', 'USGA']
+      can :manage_student, Student, :major => ['MARS', 'OCRE', 'USGA']
       can :recommend_appeal, AppealRecommendation, :major => ['MARS', 'OCRE', 'USGA']
       can :manage_address, Address, :major => ['MARS', 'OCRE', 'USGA']
     end
     
     if user.has_role? :gacd
-      # can :manage_student, Student, :major => ['GACD', 'MAST']
+      can :manage_student, Student, :major => ['GACD', 'MAST']
       can :recommend_appeal, AppealRecommendation, :major => ['GACD', 'MAST']
       can :manage_address, Address, :major => ['GACD', 'MAST']
     end
     
     if user.has_role? :mase
-      # can :manage_student, Student, :major => ['MASE', 'MASL']
+      can :manage_student, Student, :major => ['MASE', 'MASL']
       can :recommend_appeal, AppealRecommendation, :major => ['MASE', 'MASL']
       can :manage_address, Address, :major => ['MASE', 'MASL']
     end
     
     if user.has_role? :mara
-      # can :manage_student, Student, :major => 'MARA'
+      can :manage_student, Student, :major => 'MARA'
       can :recommend_appeal, AppealRecommendation, :major => 'MARA'
       can :manage_address, Address, :major => 'MARA'
     end
     
     if user.has_role? :mart
-      # can :manage_student, Student, :major => 'MART'
+      can :manage_student, Student, :major => 'MART'
       can :recommend_appeal, AppealRecommendation, :major => 'MART'
       can :manage_address, Address, :major => 'MART'
     end
